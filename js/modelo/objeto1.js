69322059663599e68b543e34155ba4ef84f0729f
Gerson Penha <gerson.penha@fatec.sp.gov.br>
2022-03-08 17:25:57 -0300

    Atualização do repositorio, apenas com código .ts

--- js/modelo/objeto1.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Objeto1 {
-    metodoComum() {
-        console.log(`Sou um objeto 1 e Meus atributos são:`);
-        console.log(`Atributo 1: ${this.atributo1}, Atributo 2: ${this.atributo2}`);
-    }
-}
-exports.default = Objeto1;
